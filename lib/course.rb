b4cb8fce2e8189d8abb87e95ffc55d5c9e729174
Marc Steele <marcsteele2014@gmail.com>
2020-11-02 21:03:56 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,4 +2,16 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
+  @@all = []
+
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+
+  def self.all
+
+
 end
